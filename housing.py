218a389d23060686cdcd4371fcb7ed1a4d9ae17d
Spencer Grady <spencergrady12@gmail.com>
2022-10-12 19:11:47 -0500

    Add files via upload

---
+++ housing.py
@@ -0,0 +1,327 @@
+import os
+import tarfile
+import urllib
+import urllib.request
+
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
+HOUSING_PATH = os.path.join("datasets", "housing")
+HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+
+#Function to get data from URL each time, helpful if dataset values change in real world
+#makes directory on your system to extract the .tgz
+def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+    if not os.path.isdir(housing_path):
+        os.makedirs(housing_path)
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
+fetch_housing_data()
+
+#load the data pulled from the url into the housing variable
+
+import pandas as pd
+def load_housing_data(housing_path=HOUSING_PATH):
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
+
+housing = load_housing_data()
+
+# print(hosing.info())  	 			Get description of data
+# print(housing.describe())	 			Get statistics of data
+# print(housing["ocean_proximity"].value_counts())	Look at details of ocean proximity
+
+import matplotlib.pyplot as plt
+# Create Histogram to visually see data
+# housing.hist(bins=50, figsize=(20,15))
+# plt.show()
+
+import numpy as np
+# Go ahead and split off the test data for after the model is created and trained
+# This function will create a new test set each time, we want one test set that the model will never see before its tested
+def split_train_test(data, test_ratio):
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+
+#create hash, doesnt work for this dataset because there is no identifier column
+from zlib import crc32
+def test_set_check(identifier, test_ratio):
+    return crc32(np.int64(identifier)) & 0xffffffff < test_ratio * 2**32
+
+#function to split test and training data on an ID of the dataset
+def split_train_test_by_id(data, test_ratio, id_column):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
+    return data.loc[~in_test_set], data.loc[in_test_set]
+
+# test function with a created index column (option1)
+
+# housing_with_id = housing.reset_index() # adds an `index` column
+# train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "index")
+# print(len(train_set))
+# print(len(test_set))
+
+#use lat/long position as unique ID, (option 2)
+# housing_with_id["id"] = housing["longitude"] * 1000 + housing["latitude"]
+# train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "id")
+# print(len(train_set))
+# print(len(test_set))
+
+#sklearn provides function to split data for us
+from sklearn.model_selection import train_test_split
+train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
+# print(len(test_set))
+# print(len(train_set))
+
+#Inspect the median income and create a histogram showing where most median incomes lie
+housing["income_cat"] = pd.cut(housing["median_income"],
+    bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
+    labels=[1, 2, 3, 4, 5])
+# housing["income_cat"].hist()
+# plt.show()
+
+from sklearn.model_selection import StratifiedShuffleSplit
+
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+
+for train_index, test_index in split.split(housing, housing["income_cat"]):
+    strat_train_set = housing.loc[train_index]
+    strat_test_set = housing.loc[test_index]
+# print(strat_test_set["income_cat"].value_counts() / len(strat_test_set))
+
+#get data back to original state
+for set_ in (strat_train_set, strat_test_set):
+    set_.drop("income_cat", axis=1, inplace=True)
+
+#create a copy of the test set
+housing = strat_train_set.copy()
+#create detailed plot to visualize data and geography
+# red is expensive, blue is less expensive. larger circle means more population
+housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.4,
+ s=housing["population"]/100, label="population", figsize=(10,7),
+ c="median_house_value", cmap=plt.get_cmap("jet"), colorbar=True,
+)
+plt.legend()
+# plt.show()
+
+
+corr_matrix = housing.corr()
+# print(corr_matrix["median_house_value"].sort_values(ascending=False))
+
+from pandas.plotting import scatter_matrix
+#scatter matrix will compare multiple attributes at once
+attributes = ["median_house_value", "median_income", "total_rooms","housing_median_age"]
+scatter_matrix(housing[attributes], figsize=(12, 8))
+housing.plot(kind="scatter", x="median_income", y="median_house_value",alpha=0.1)
+
+# plt.show()
+#mess with the data to get better information: IE combining attributes that are less meaningful alone
+housing["rooms_per_household"] = housing["total_rooms"]/housing["households"]
+housing["bedrooms_per_room"] = housing["total_bedrooms"]/housing["total_rooms"]
+housing["population_per_household"]=housing["population"]/housing["households"]
+
+corr_matrix = housing.corr()
+# print(corr_matrix["median_house_value"].sort_values(ascending=False))
+
+
+# PREPARE DATA FOR ML ALGORITHM #
+# create a clean training set
+housing = strat_train_set.drop("median_house_value", axis=1)
+housing_labels = strat_train_set["median_house_value"].copy()
+
+#total bedrooms is missing some entries, so replace missing values with the median value
+
+# housing.drop("total_bedrooms", axis=1) # option 2
+median = housing["total_bedrooms"].median() # option 3
+housing["total_bedrooms"].fillna(median, inplace=True)
+
+#sci kit learn has function that will replace missing values with the median for us (imputer)
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(strategy="median")
+
+housing_num = housing.drop("ocean_proximity", axis=1)
+
+imputer.fit(housing_num)
+# print(imputer.statistics_)
+
+X = imputer.transform(housing_num)
+housing_tr = pd.DataFrame(X, columns=housing_num.columns)
+housing_cat = housing[["ocean_proximity"]]
+# print(housing_cat.head(10))
+
+# Ordinal Encoder can translate text attributes to a corresponding number
+from sklearn.preprocessing import OrdinalEncoder
+ordinal_encoder = OrdinalEncoder()
+
+housing_cat_encoded = ordinal_encoder.fit_transform(housing_cat)
+# these print statmenets show which numbers align with which attributes
+# print(housing_cat_encoded[:10])
+# print(ordinal_encoder.categories_)
+#issue: It assumes the closer the assigned numbers, the closer the text attributes which isnt always true
+
+#HotEncoder will assign a 1 or 0. 
+from sklearn.preprocessing import OneHotEncoder
+cat_encoder = OneHotEncoder()
+housing_cat_1hot = cat_encoder.fit_transform(housing_cat)
+# print(housing_cat_1hot)
+# print(housing_cat_1hot.toarray())
+# print(cat_encoder.categories_)
+
+
+# TRANSFORMERS#
+#write a transformer to combine the attributes and tell wehter or not they will help our model
+from sklearn.base import BaseEstimator, TransformerMixin
+rooms_ix, bedrooms_ix, population_ix, households_ix = 3, 4, 5, 6
+class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
+    def __init__(self, add_bedrooms_per_room = True): # no *args or **kargs
+        self.add_bedrooms_per_room = add_bedrooms_per_room
+    def fit(self, X, y=None):
+        return self # nothing else to do
+    def transform(self, X, y=None):
+        rooms_per_household = X[:, rooms_ix] / X[:, households_ix]
+        population_per_household = X[:, population_ix] / X[:, households_ix]
+        if self.add_bedrooms_per_room:
+            bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
+            return np.c_[X, rooms_per_household, population_per_household,bedrooms_per_room]
+        else:
+            return np.c_[X, rooms_per_household, population_per_household]
+
+#Hyperparameter add_bedrooms_per_room set to true to tell if it will help or hurt the model
+#Can tune different combinations to find the best fit for your model
+attr_adder = CombinedAttributesAdder(add_bedrooms_per_room=False)
+housing_extra_attribs = attr_adder.transform(housing.values)
+
+#PIPELINE#
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+num_pipeline = Pipeline([
+ ('imputer', SimpleImputer(strategy="median")),
+ ('attribs_adder', CombinedAttributesAdder()),
+ ('std_scaler', StandardScaler()),
+ ])
+housing_num_tr = num_pipeline.fit_transform(housing_num)
+
+#ColumnTransfer can handle text and numerical attributes at the same time, using the OneHotEncoder method
+from sklearn.compose import ColumnTransformer
+num_attribs = list(housing_num)
+cat_attribs = ["ocean_proximity"]
+full_pipeline = ColumnTransformer([
+ ("num", num_pipeline, num_attribs),
+ ("cat", OneHotEncoder(), cat_attribs),
+ ])
+housing_prepared = full_pipeline.fit_transform(housing)
+
+#Models To Try#
+# LINEAR REGRESSION MODEL
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(housing_prepared, housing_labels)
+
+some_data = housing.iloc[:5]
+some_labels = housing_labels.iloc[:5]
+some_data_prepared = full_pipeline.transform(some_data)
+# print("Predictions:", lin_reg.predict(some_data_prepared))
+# print("Labels:", list(some_labels))
+# for real world data, linear regression usually isnt the best option becasue of complexity of data sets
+
+from sklearn.metrics import mean_squared_error
+
+housing_predictions = lin_reg.predict(housing_prepared)
+lin_mse = mean_squared_error(housing_labels, housing_predictions)
+lin_rmse = np.sqrt(lin_mse)
+# error of the prediction in dollars
+# print(lin_rmse)
+
+
+# DECISION TREE MODEL
+from sklearn.tree import DecisionTreeRegressor
+tree_reg = DecisionTreeRegressor()
+tree_reg.fit(housing_prepared, housing_labels)
+
+housing_predictions = tree_reg.predict(housing_prepared)
+tree_mse = mean_squared_error(housing_labels, housing_predictions)
+tree_rmse = np.sqrt(tree_mse)
+print(tree_rmse)
+#Example of overfitting the data with the error of ZERO
+
+#cross validation will run the model 'cv' number of times, we can get an array of the errors from each run
+from sklearn.model_selection import cross_val_score
+scores = cross_val_score(tree_reg, housing_prepared, housing_labels,scoring="neg_mean_squared_error", cv=10)
+tree_rmse_scores = np.sqrt(-scores)
+
+#write small function to display the statistics
+def display_scores(scores):
+    print("Scores:", scores)
+    print("Mean:", scores.mean())
+    print("Std Dev:", scores.std())
+
+display_scores(tree_rmse_scores)
+
+#linear regression scores
+lin_scores = cross_val_score(lin_reg, housing_prepared, housing_labels, scoring="neg_mean_squared_error", cv=10)
+lin_rmse_scores = np.sqrt(-lin_scores)
+display_scores(lin_rmse_scores)
+
+
+#RANDOM FOREST REGRESSOR
+#trains on many decision trees on random attributes and averaging the predictions
+from sklearn.ensemble import RandomForestRegressor
+forest_reg = RandomForestRegressor()
+forest_reg.fit(housing_prepared, housing_labels)
+
+housing_predictions = forest_reg.predict(housing_prepared)
+forest_mse = mean_squared_error(housing_labels, housing_predictions)
+forest_rmse = np.sqrt(forest_mse)
+
+forest_scores = cross_val_score(forest_reg, housing_prepared, housing_labels, scoring="neg_mean_squared_error", cv=10)
+forest_rmse_scores = np.sqrt(-forest_scores)
+display_scores(forest_rmse_scores)
+
+#grid search to fine tune parameters
+from sklearn.model_selection import GridSearchCV
+
+param_grid = [{'n_estimators': [3, 10, 30], 'max_features': [2, 4, 6, 8]},
+    {'bootstrap': [False], 'n_estimators': [3, 10], 'max_features': [2, 3, 4]},
+]
+
+forest_reg = RandomForestRegressor()
+grid_search = GridSearchCV(forest_reg, param_grid, cv=5,scoring='neg_mean_squared_error',return_train_score=True)
+grid_search.fit(housing_prepared, housing_labels)
+
+print(grid_search.best_params_)
+print(grid_search.best_estimator_)
+
+#evaluation scores
+cvres = grid_search.cv_results
+for mean_score, params in zip(cvres["mean_test_score"], cvres["params"]):
+    print(np.sqrt(-mean_score), params)
+
+#go back and try randomized search
+feature_importances = grid_search.best_estimator_.feature_importances
+
+extra_attribs = ["rooms_per_hhold", "pop_per_hhold", "bedrooms_per_room"]
+cat_encoder = full_pipeline.named_transformers_["cat"]
+cat_one_hot_attribs = list(cat_encoder.categories_[0])
+attributes = num_attribs + extra_attribs + cat_one_hot_attribs
+print(sorted(zip(feature_importances, attributes), reverse=True))
+
+
+final_model = grid_search.best_estimator_
+X_test = strat_test_set.drop("median_house_value", axis=1)
+y_test = strat_test_set["median_house_value"].copy()
+X_test_prepared = full_pipeline.transform(X_test)
+final_predictions = final_model.predict(X_test_prepared)
+final_mse = mean_squared_error(y_test, final_predictions)
+final_rmse = np.sqrt(final_mse) # => evaluates to 47,730.2
+
+from scipy import stats
+confidence = 0.95
+squared_errors = (final_predictions - y_test) ** 2
+print(np.sqrt(stats.t.interval(confidence, len(squared_errors) - 1,loc=squared_errors.mean(),scale=stats.sem(squared_errors))))
+
+
